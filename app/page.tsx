18d077b457b2f8ad2028bda174794b78767287a3
k4in <>
2024-07-11 01:05:43 +0200

    Inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 
 import RowGuess from '@/components/row-guess';
 
--- components/row-guess.tsx
+++ components/row-guess.tsx
@@ -1,14 +1,23 @@
 import { WordLineType } from '@/app/page';
 
+import { Input } from './ui/input';
+
 export default function RowGuess({ data }: { data: WordLineType }) {
   const rowLength = Object.keys(data).length;
+
+  console.log(data);
 
   return (
     <div className="flex gap-3">
       {Array.from({ length: rowLength }, (_, i) => i).map((i) => (
-        <div key={i} className="h-20 w-20 border border-foreground flex justify-center items-center">
-          {JSON.stringify(data[i])}
-        </div>
+        // <div key={i} className="h-20 w-20 border border-foreground flex justify-center items-center">
+        //   {JSON.stringify(data[i])}
+        // </div>
+        <Input
+          key={i}
+          className="h-20 w-20 border border-foreground flex justify-center items-center text-center text-3xl"
+          type="text"
+        />
       ))}
     </div>
   );
